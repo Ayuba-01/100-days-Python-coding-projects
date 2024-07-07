a075a2ac3ee6c85f0e30b2a5f1ee427acb32e8b8
ayuba <yusufayuba.a11@gmail.com>
2024-07-07 12:10:32 +0100

    Ceaser Cipher code simplified

--- ceaser_cipher.py
+++ ceaser_cipher.py
@@ -4,27 +4,24 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(plain_text, shift_amount):
-    cipher_text = ""
-    for letter in plain_text:
-        position = alphabet.index(letter)
-        new_position = position + shift_amount
-        cipher_text += alphabet[new_position]
-    print(f"The encoded text is {cipher_text}")
 
-def decrypt(cipher_text, shift_amount):
-    plain_text = ""
-    for letter in cipher_text:
+def ceaser(text, shifts, direction):
+    final_text = ""
+    if direction != "encode" or direction != "decode":
+            print("You didn't type encode or decode.")
+    for letter in text:
         position = alphabet.index(letter)
-        new_position = position - shift_amount
-        plain_text += alphabet[new_position]
-    print(f"The decoded text is {plain_text}")
+        if direction == "encode":
+            new_position = position + shifts
+        elif direction == "decode":
+            new_position = position - shifts
+        final_text += alphabet[new_position]
+    print(f"The {direction}d text is {final_text}")
+
+
+ceaser(text, shift, direction)
+
+    
     
 
 
-if direction == "encode":
-    encrypt(plain_text=text, shift_amount=shift)
-elif direction == "decode":
-    decrypt(cipher_text=text, shift_amount=shift)
-else:
-    print("You didn't type encode or decode.")
