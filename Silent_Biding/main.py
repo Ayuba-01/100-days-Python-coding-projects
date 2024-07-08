40e53e502e81bffcf18c9d06805555dd3a46fbbc
ayuba <yusufayuba.a11@gmail.com>
2024-07-08 12:28:55 +0100

    Silent Bidding app

---
+++ Silent_Biding/main.py
@@ -0,0 +1,26 @@
+import os
+from logo import logo
+
+
+def getBidWinner(dic):
+    max_key = max(dic, key=dic.get)
+    print(f"The highest bider is {max_key} with the bid of ${dic[max_key]}.")
+    
+
+print(logo)
+bid_dic = {}
+more_bids = True
+while more_bids:
+    name = input("What is your name? ")
+    price = input("How much are you willing to pay $")
+    bid_dic[name] = price
+    answer = input("Type 'yes' if there is more bids or 'no' if you are the final bider: ")
+    os.system('clear') 
+    if answer == "no":
+        more_bids = False
+        getBidWinner(bid_dic)
+       
+
+
+         
+    
