db690e0fea128101e842991b3926269bdec6c658
ayuba <yusufayuba.a11@gmail.com>
2024-07-20 09:44:00 +0100

    Hangman and Slient Bididng updates

--- Silent_Biding/main.py
+++ Silent_Biding/main.py
@@ -4,7 +4,7 @@ from logo import logo
 
 def getBidWinner(dic):
     max_key = max(dic, key=dic.get)
-    print(f"The highest bider is {max_key} with the bid of ${dic[max_key]}.")
+    print(f"The highest bidder is {max_key} with the bid of ${dic[max_key]}.")
     
 
 print(logo)
@@ -14,7 +14,7 @@ while more_bids:
     name = input("What is your name? ")
     price = input("How much are you willing to pay $")
     bid_dic[name] = price
-    answer = input("Type 'yes' if there is more bids or 'no' if you are the final bider: ")
+    answer = input("Type 'yes' if there is more bids or 'no' if you are the final bidder: ")
     os.system('clear') 
     if answer == "no":
         more_bids = False
