e9d39da142ee125257d0fa4f3b9d0ab34bddde7c
ayuba <yusufayuba.a11@gmail.com>
2024-07-24 10:01:14 +0100

    Calculator Inmproved

--- simple_calculator/main.py
+++ simple_calculator/main.py
@@ -20,21 +20,22 @@ operations = {"+" : addition, "-" : subtraction, "/" : division, "*" : multiplic
 def calculator():
     print(logo)
     again = True
-    num1 = int(input("Enter first number: "))
+    num1 = float(input("Enter first number: "))
     while again:
         for key in operations:
             print(key)
         operation = input("choose operation: ")
-        num2 = int(input("Enter next number: "))
+        num2 = float(input("Enter next number: "))
         calc_functions = operations[operation]
         result = calc_functions(num1, num2)
         print(f"{num1} {operation} {num2} = {result}")
-        answer = input("Do you want to continue calculation with your previous answer. Enter 'y' for Yes and 'n' for No.: ").lower()
+        answer = input("Do you want to start a new calculation. Enter 'y' for Yes and 'n' for No.: ").lower()
         if answer == "n":
             again = False
             print("Thank you!")
         else:
             num1 = result
+            calculator()
         
 
 
