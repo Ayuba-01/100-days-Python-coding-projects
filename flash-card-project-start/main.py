988a1c49fb2c75651063530ce6ea9e4f9c2296fd
ayuba <yusufayuba.a11@gmail.com>
2024-10-11 12:09:46 +0100

    Flash card flipping mechanish done

--- flash-card-project-start/main.py
+++ flash-card-project-start/main.py
@@ -3,41 +3,42 @@ import pandas
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+random_dict = {}
 
 # ---------------------------- change word mechanism ------------------------------- #
 with open("./data/french_words.csv") as file:
     f = pandas.read_csv(file)
     df = pandas.DataFrame(f)
     data_dict_list = df.to_dict(orient="records")
-    print(data_dict_list)
 
 
 def next_card():
+    global random_dict, flip_timer
+    window.after_cancel(flip_timer)
     random_dict = random.choice(data_dict_list)
-    random_french_word = random_dict["French"]
-    canvas.itemconfigure(word_label, text=f"{random_french_word}")
-    canvas.itemconfigure(lang_label, text=f"French")
+    canvas.itemconfigure(word_label, text=f"{random_dict['French']}", fill="black")
+    canvas.itemconfigure(lang_label, text=f"French", fill="black")
+    canvas.itemconfigure(canvas_image, image=front_card_image)
+    flip_timer = window.after(3000, flip_card)
 
 
-def wrong_button_function():
-    random_dict = random.choice(data_dict_list)
-    random_french_word = random_dict["French"]
-    canvas.itemconfigure(word_label, text=f"{random_french_word}")
-
-
-def right_button_function():
-    random_dict = random.choice(data_dict_list)
-    random_french_word = random_dict["French"]
-    canvas.itemconfigure(word_label, text=f"{random_french_word}")
+# ---------------------------- Flip card mechanism ------------------------------- #
+def flip_card():
+    global random_dict
+    canvas.itemconfigure(canvas_image, image=back_card_image)
+    canvas.itemconfigure(lang_label, text="English", fill="white")
+    canvas.itemconfigure(word_label, text=random_dict["English"], fill="white")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+flip_timer = window.after(3000, flip_card)
 canvas = Canvas(width=800, height=530, highlightthickness=0, bg=BACKGROUND_COLOR)
 front_card_image = PhotoImage(file="./images/card_front.png")
-canvas.create_image(400, 275, image=front_card_image)
+back_card_image = PhotoImage(file="./images/card_back.png")
+canvas_image = canvas.create_image(400, 275, image=front_card_image)
 lang_label = canvas.create_text(400, 150, text="", fill="black", font=("Ariel", 40, "italic"))
 word_label = canvas.create_text(400, 263, text="", fill="black", font=("Ariel", 60, "bold"))
 canvas.grid(column=0, row=0, columnspan=2)
